d2cb1e65f540d2914271df397aba57a425007f82
Will Riffe <w_riffe@protonmail.com>
2023-03-27 18:30:48 -0400

    straightened out most of the JavaScript...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,34 @@
 // Get the current date and display it at the top of the calendar
-var currentDate = moment().format('dddd, MMMM Do');
+var currentDate = moment().format('dddd, MMMM Do, h:mm a');
 $("#currentDay").text(currentDate);
 
-// Color-code the time blocks based on whether they are in the past, present, or future
-$(".time-block").each(function () {
-  var currentHour = moment().hours();
-  var blockHour = parseInt($(this).attr("id"));
-
-  if (blockHour < currentHour) {
-    $(this).addClass("past");
-  } else if (blockHour === currentHour) {
-    $(this).removeClass("past");
-    $(this).addClass("present");
-  } else {
-    $(this).removeClass("past");
-    $(this).removeClass("present");
-    $(this).addClass("future");
-  }
-});
+// Allows color-coding new classes: past, present, or future
+$(".time-block tr").each(function () {
+    var currentHour = moment().hours();
+    var blockHour = parseInt($(this).attr("id").replace("hour", ""));
+  
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+  
 
 // Save the text for each event to local storage when the save button is clicked
 $(".saveBtn").on("click", function () {
-  var blockId = $(this).parent().attr("id");
-  var blockText = $(this).siblings(".description").val();
-
-  localStorage.setItem(blockId, blockText);
-});
-
-// Load any saved events from local storage and display them in the time blocks
-$(".time-block").each(function () {
-  var blockId = $(this).attr("id");
-  var blockText = localStorage.getItem(blockId);
-
-  $(this).children(".description").val(blockText);
-});
+    var blockId = $(this).closest("tr").attr("id");
+    var blockText = $("#" + blockId + " input").val();
+  
+    localStorage.setItem(blockId, blockText);
+  });
+  
+  // Load any saved events from local storage and display them in the time blocks
+  $(document).ready(function () {
+    var blockText = localStorage.getItem(blockId);
+      
+    $(this).closest("tr").children("input").val(blockText);
+  });
+  
